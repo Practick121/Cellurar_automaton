4615094fcd2218106dc55186aaa13c3b5ff875f3
Practick121 <122594142+Practick121@users.noreply.github.com>
2024-03-13 00:07:42 +0300

    Update defenition.py

--- defenition.py
+++ defenition.py
@@ -1,18 +1,14 @@
 #   импорт всех модулей
-
 from pygame import Surface, Rect
-from pygame import QUIT, MOUSEBUTTONUP, MOUSEBUTTONDOWN, FULLSCREEN, DOUBLEBUF
+from pygame import QUIT, MOUSEBUTTONUP, MOUSEBUTTONDOWN, DOUBLEBUF
 from pygame import time, display, event, mouse
 from pygame import init as pygame_init
 from pygame import font
-
 from random import randint
-
 import sys
 
-
 #    константы
-GLOBAL_SIZE = (1000, 750)
+GLOBAL_SIZE = (900, 750)
 TABLE_SIZE = (750, 750)
 TITLE = "Клеточный автомат"
 font_list = {'normal': (None, 15),
@@ -27,26 +23,25 @@ CLEAN = 3
 GENERATE = 4
 
 #    цвета
-
 GRAY = (128, 128, 128)
+GREEN = (0, 204, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BRIGHT_BLUE = (55, 155, 255)
 
-
 #    остальные константы
-DELAY = 0.1  # задержка между изменениями таблицы
-SPEED_RUN = 5  # количество дней в секунду - не работает
-NUMBER_OF_CELLS = 100
-FPS = 100  # количество кадров в секунду
-IPS = 100  # количество итераций в секунду
-CHANCE = 20  # шанс на появление живой клетки при генерации %
+NAME = 'data.txt'  # имя открываемого файла
+GRID = True  # True - сетка отображается
+# (даже при False сетка может отображаться из-за погрешности размера клетки)
+LIVE_COLOR = BLACK  # цвет живых клеток
+DEATH_COLOR = WHITE  # цвет мертвых клеток
+DELAY = 0.05  # задержка между изменениями таблицы
+NUMBER_OF_CELLS = 100  # количество клеток на одной стороне квадрата
+FPS = 60  # количество кадров в секунду
+IPS = 10000  # количество итераций в секунду
+CHANCE = 50  # шанс на появление живой клетки при генерации (%)
 Birth, Survive = [5, 6, 7, 8], [4, 5, 6, 7, 8]
 # кол-во живых соседей для зарождения жизни, кол-во живых соседей для выживания
 TYPE_NEIGHBORS = "8"  # 8 - по сторонам и диагоналям, 4 - по сторонам
-
-# 3/12345 - лабиринт
-# 3/23 - жизнь
-# 5678/45678 - пещеры
