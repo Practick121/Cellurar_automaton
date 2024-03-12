f779a010e49f0c2f8883881e94ec18b85e3cd9f9
Practick121 <122594142+Practick121@users.noreply.github.com>
2024-03-13 00:08:02 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 from defenition import RUN, STOP, GENERATE, RUN_ONE_STEP, CLEAN, MOUSEBUTTONUP, MOUSEBUTTONDOWN, QUIT, WHITE
-from defenition import event, mouse, display, sys
+from defenition import event, display, sys, NAME
 from defenition import Surface
-import defenition
 from Classes import Transmission, Button, Table
 
 
@@ -10,6 +9,7 @@ def processing_event():
         if ev.type == QUIT:
             close_prog()
         if ev.type == MOUSEBUTTONDOWN:
+            Table.last_ev = ev
             for button in Button.list:
                 button.press_down()
         if ev.type == MOUSEBUTTONUP:
@@ -17,12 +17,8 @@ def processing_event():
                 button.press_up()
 
 
-def processing_mouse_events():
-    if mouse.get_pressed():
-        pass
-
-
 def processing_own_events():
+    # распределение общих событий
     if Transmission.gl_status == RUN and Transmission.delay_timer.is_out():
         Table.status = RUN
         Transmission.delay_timer.wind_up()
@@ -36,13 +32,14 @@ def processing_own_events():
         Table.status = STOP
     elif Transmission.gl_status == CLEAN:
         Table.status = CLEAN
+        Transmission.gl_status = STOP
 
 
 def upload_file():
     i = 1
     while i <= 100:
         try:
-            file = open(f'data{i}', 'x')
+            file = open(f'data{i}.txt', 'x')
             break
         except FileExistsError:
             i += 1
@@ -50,25 +47,41 @@ def upload_file():
         print('Не удалось создать файл')
         return
     for table in Table.tables:
-        file.write('[')
+        file.write('[\n')
         for string in table.matrix:
             file.write(', '.join([str(elem) for elem in string]))
             file.write('\n')
         file.write(']')
     file.close()
+    print('Файл выгружен.\n Для просмотра закройте программу')
 
 
 def open_file():
     try:
-        file = open('data', 'r')
-    except FileExistsError:
-        print('Не удалось открыть файл')
+        file = open(NAME, 'r')
+    except FileNotFoundError as exc:
+        print(exc)
         return
-    file.read()
-    file.split('\n')
-    for i in range(len(Table.tables[0])):
-        for j in range(len(Table.tables[0])):
-            Table.tables[0].matrix[i][j] = file[i][j]
+    try:
+        mas1 = file.readlines()
+        size = len(mas1)
+        i = -2
+        for line in mas1:
+            i += 1
+            mas2 = line.split(', ')
+            if i == -1 or i == size - 2:
+                continue
+            j = -1
+            for elem in mas2:
+                j += 1
+                if elem[0] == '0':
+                    Table.tables[0].matrix[i][j] = 0
+                else:
+                    Table.tables[0].matrix[i][j] = 1
+    except IndexError as exc:
+        print(exc, '\nРазмер открываемого файла превышает размер таблицы')
+        return
+    print('Файл открыт успешно')
 
 
 def to_run():
@@ -77,17 +90,17 @@ def to_run():
 
 def to_clean():
     Transmission.gl_status = CLEAN
-    Transmission.paintstop = True
+    Transmission.paint_stop = True
 
 
 def to_generate():
     Transmission.gl_status = GENERATE
-    Transmission.paintstop = True
+    Transmission.paint_stop = True
 
 
 def to_one_step():
     Transmission.gl_status = RUN_ONE_STEP
-    Transmission.paintstop = True
+    Transmission.paint_stop = True
 
 
 def to_stop():
@@ -97,11 +110,6 @@ def to_stop():
 def rendering(window, sprites):
     for sprite in sprites:
         sprite.render(window)
-    display.flip()
-
-
-def first_rendering(window, sprites):
-    rendering(window, sprites)
     display.flip()
 
 
